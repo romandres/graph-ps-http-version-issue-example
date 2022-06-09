a9bc6a8bc7b94b2050d1632957a65f7fb76218fb
Roman Andres <roman@ndres.dev>
2022-06-09 15:00:42 +0200

    Add example

---
+++ src/GraphPowerShellHttpVersionIssue/GraphHelper.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+
+namespace GraphPowerShellHttpVersionIssue
+{
+    internal class GraphHelper
+    {
+        private readonly IEnumerable<string> scopes = new List<string>()
+        {
+            "https://graph.microsoft.com/.default"
+        };
+
+        private readonly IConfidentialClientApplication clientApp;
+
+        public GraphHelper(string tenantId, string clientId, string clientSecret)
+        {
+            clientApp = CreateClientApp(tenantId, clientId, clientSecret);
+        }
+
+        public async Task<GraphServiceClient> CreateGraphServiceClientAsync()
+        {
+            var authenticationResult = await clientApp
+                    .AcquireTokenForClient(scopes)
+                    .ExecuteAsync()
+                    .ConfigureAwait(false);
+
+            var graphServiceClient = new GraphServiceClient(
+                    "https://graph.microsoft.com/v1.0",
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+                    new DelegateAuthenticationProvider(async (requestMessage) =>
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+                    {
+                        requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", authenticationResult.AccessToken);
+                    })
+                );
+
+            return graphServiceClient;
+        }
+
+        private IConfidentialClientApplication CreateClientApp(string tenantId, string clientId, string clientSecret)
+        {
+            var clientAppBuilder = ConfidentialClientApplicationBuilder
+                .Create(clientId)
+                .WithAuthority(AadAuthorityAudience.AzureAdMyOrg)
+                .WithTenantId(tenantId)
+                .WithClientSecret(clientSecret);
+
+            return clientAppBuilder.Build();
+        }
+    }
+}
---
+++ src/GraphPowerShellHttpVersionIssue/TestGraphPowerShellHttpVersionIssueCmdlet.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Management.Automation;
+using System.Net;
+using System.Security;
+using Microsoft.Graph;
+
+namespace GraphPowerShellHttpVersionIssue
+{
+    [Cmdlet(VerbsDiagnostic.Test, "GraphPowerShellHttpVersionIssue")]
+    public class TestGraphPowerShellHttpVersionIssueCmdlet : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string ApplicationId { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public SecureString ApplicationSecret { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string TenantId { get; set; }
+
+        protected override void EndProcessing()
+        {
+            try
+            {
+                var appCredential = new NetworkCredential(ApplicationId, ApplicationSecret);
+                var graphClient = (new GraphHelper(TenantId, appCredential.UserName, appCredential.Password))
+                    .CreateGraphServiceClientAsync()
+                    .GetAwaiter()
+                    .GetResult(); ;
+
+                // This works with Graph v3.x and PowerShell as well as Windows PowerShell
+                // This works with Graph v4.x and PowerShell but not with Windows PowerShell
+
+                var groups = graphClient
+                    .Groups
+                    .Request()
+                    .GetAsync()
+                    .GetAwaiter()
+                    .GetResult()
+                    .Select(group => group.DisplayName)
+                    .ToList();
+
+                WriteObject(groups);
+            }
+            catch (ServiceException ex)
+            {
+                // The interesting error message is on the inner exception
+                if (ex.InnerException?.Message is object && ex.InnerException.Message.Contains("HTTP/1.0"))
+                {
+                    WriteWarning(ex.InnerException.Message);
+                }
+
+                ThrowTerminatingError(new ErrorRecord(ex, "", ErrorCategory.NotSpecified, null));
+            }
+        }
+    }
+}
